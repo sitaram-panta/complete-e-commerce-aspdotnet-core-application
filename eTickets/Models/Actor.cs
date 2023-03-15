3fe0457f91753ddf870c40cffe0a2d05e2aed866
sitaram-panta <117626335+sitaram-panta@users.noreply.github.com>
2023-03-15 10:35:58 +0545

    Created relationship in model

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int ID { get; set; }
+        public string ProfilePicturURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+    }
+}
---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using eTickets.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public String ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+
+        //Relationship
+        public List<Actor_Movie> Actors_Movies { get; set; }
+        
+        //cinema
+        public int CinemaId { get; set; }
+        [ForeignKey("CinemaID")]
+        public Cinema Cinema { get; set; }
+
+        //producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerID")]
+        public Producer Producer { get; set; }
+    }
+}
